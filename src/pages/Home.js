2ed383d44237885d765365f7c5700177142e8cdd
Varun Goel <varun.goel@aviva.com>
2019-06-14 17:52:32 +0800

    subscribe to incidents

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Incident } from '../components/Incident';
 
-export function Home(props) {
-  return (
-    <div>
-      <Incident
-        title="Test incident"
-        assignee="Admin"
-        status="Resolved"
-      />
-      <Incident
-        title="Another incident"
-        assignee="Engineer"
-        status="Acknowledged"
-      />
-    </div>
-  )
+export class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.props.store.subscribe(this.render);
+  }
+
+  render() {
+    let { store } = this.props;
+    let incidents = store.getState().map(incident => {
+      return (<Incident
+        title={incident.name}
+        assignee={incident.assignee}
+        status={incident.status}
+      />)
+    });
+
+    return (
+      <div>
+        {incidents}
+      </div>
+    )
+  }
+
 }
