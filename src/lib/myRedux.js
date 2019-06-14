6204cf3daa9644fa8af8b734cf78aea075fd13a8
Varun  Goel <codebitconsulting@gmail.com>
2019-06-15 01:10:34 +0800

    Added code comments for myRedux lib

--- src/lib/myRedux.js
+++ src/lib/myRedux.js
@@ -3,13 +3,16 @@ const createStore = (reducer) => {
     let listeners = [];
     let state;
 
+    // returns the application state
     const getState = () => state;
 
+    //  to calculate the new state we call the reducer with the current state and the action being dispatched. After the state was updated, we need to notify every changed listener, by calling it 
     const dispatch = (action) => {
         state = reducer(state, action);
         listeners.forEach(listener => listener());
     }
 
+    // stores all listeners and provide a way to unsubscribe a listener
     const subscribe = (listener) => {
         listeners.push(listener);
         return (() => {
@@ -17,6 +20,7 @@ const createStore = (reducer) => {
         })
     }
 
+    // set the initail state of application by passing empty object so that the reducer retuurns default state
     dispatch({});
 
     return { subscribe, getState, dispatch };
